a3a0026c94806eebc43722373b6277fdda7fc708
bobbyhadz <opmftw@gmail.com>
2024-03-02 10:52:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// ⛔️ Invalid left-hand side in assignment
+
+// EXAMPLE 1 - Use double or triple equals when comparing values
+
+// ✅ correct
+if (7 === 7) {
+  console.log('success');
+}
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Use bracket notation for object properties that contain hyphens
+
+// // ✅ correct
+// const obj = {};
+
+// obj['background-color'] = 'green';
+
+// console.log(obj);
